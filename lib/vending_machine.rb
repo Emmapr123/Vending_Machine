0a8c115f6032f16dc9166784b4a58c10afd5124a
Emmapr123 <emmapriester@hotmail.com>
2021-02-18 15:14:54 +0000

    initializer

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,3 +1,17 @@
-  class VendingMachine
+# Vending Machine
+# Create a class called VendingMachine that represents a vending machine for some product. A VendingMachine object returns strings describing its interactions.
+
+# Vending Machine must keep track of the inventory
+# A person should be able to insert cash into the machine & choose an item
+# The Machine should confirm the inserted cash with the price of the selected item
+# The machine must display an error in case of insufficient cash or unavailable item
+# Finally, if all the above steps succeed then the user gets the selected item
+
+
+class VendingMachine
+  attr_accessor 'name'
+
+  def initialize(name)
+    @name = name
   end
-  
+end
--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -1,11 +1,48 @@
 require 'vending_machine'
 
 describe VendingMachine do
+  let(:candy) { VendingMachine.new 'candy' }
+
+  it 'has a getter' do
+    expect(candy.name).to eq 'candy'
+  end
+
 end
 
-# describe 'fizzbuzz' do
-#   it 'returns "fizz" when passed 3' do
-#     expect(fizzbuzz(3)).to eq 'Fizz'
-#     expect(3.fizzbuzz).to eq 'Fizz'
-#   end
-# end
+# candy = VendingMachine.new('candy')
+# candy.name ----> 'candy'
+
+# """A vending machine that vends some product for some price.
+#     >>> v = VendingMachine('candy', 10)
+#     >>> v.vend()
+#     'Inventory empty. Restocking required.'
+#     >>> v.add_funds(15)
+#     'Inventory empty. Restocking required. Here is your $15.'
+#     >>> v.restock(2)
+#     'Current candy stock: 2'
+#     >>> v.vend()
+#     'You must add $10 more funds.'
+#     >>> v.add_funds(7)
+#     'Current balance: $7'
+#     >>> v.vend()
+#     'You must add $3 more funds.'
+#     >>> v.add_funds(5)
+#     'Current balance: $12'
+#     >>> v.vend()
+#     'Here is your candy and $2 change.'
+#     >>> v.add_funds(10)
+#     'Current balance: $10'
+#     >>> v.vend()
+#     'Here is your candy.'
+#     >>> v.add_funds(15)
+#     'Inventory empty. Restocking required. Here is your $15.'
+#     >>> w = VendingMachine('soda', 2)
+#     >>> w.restock(3)
+#     'Current soda stock: 3'
+#     >>> w.restock(3)
+#     'Current soda stock: 6'
+#     >>> w.add_funds(2)
+#     'Current balance: $2'
+#     >>> w.vend()
+#     'Here is your soda.'
+#     """
