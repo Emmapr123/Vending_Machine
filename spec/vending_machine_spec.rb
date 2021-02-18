c19eb57613689785d3caa13158e0c50bf553abb0
Emmapr123 <emmapriester@hotmail.com>
2021-02-18 14:52:53 +0000

    got the rspec running

---
+++ spec/vending_machine_spec.rb
@@ -0,0 +1,11 @@
+require 'vending_machine'
+
+describe VendingMachine do
+end
+
+# describe 'fizzbuzz' do
+#   it 'returns "fizz" when passed 3' do
+#     expect(fizzbuzz(3)).to eq 'Fizz'
+#     expect(3.fizzbuzz).to eq 'Fizz'
+#   end
+# end
